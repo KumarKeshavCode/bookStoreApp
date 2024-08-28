934fefd6cd4f1ab7f0486ab241b9228cfbbf3c21
KumarKeshavCode <kumarkeshav03030@gmail.com>
2024-08-28 23:51:57 +0530

    Add: navbar complted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginSignupComponent } from './components/authentication/login-signup/login-signup.component';
+import { ForgetPasswordComponent } from './components/authentication/forget-password/forget-password.component';
+import { HeaderComponent } from './components/home/header/header.component';
+import { BookCardComponent } from './components/home/book-card/book-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginSignupComponent,
+    ForgetPasswordComponent,
+    HeaderComponent,
+    BookCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
